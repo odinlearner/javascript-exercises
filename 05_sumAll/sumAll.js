6a988f133e35ced0e9c457f072ba76f5b96a0e63
Odin <116444054+odinlearner@users.noreply.github.com>
2022-10-28 19:18:30 +0200

    Solve 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    if (typeof n1 !== "number" || typeof n2 !== "number")
+        return "ERROR"
+    if (n1 < 0 || n2 < 0) return "ERROR"
 
+    const lowerbound = Math.min(n1,n2);
+    const upperbound = Math.max(n1,n2);
+
+    let sum = 0;
+    for (let i = lowerbound; i <= upperbound; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
