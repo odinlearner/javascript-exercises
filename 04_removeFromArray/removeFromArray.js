af00414eaaaa50cc616a9f556f5c79868ba92ae9
Odin <116444054+odinlearner@users.noreply.github.com>
2022-10-28 19:09:41 +0200

    Solve 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(baseArray, ...valuesToRemove) {
+    return baseArray.filter(v => !valuesToRemove.includes(v));
 };
 
 // Do not edit below this line
