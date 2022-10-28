b4bd3e272321c0d7bc070f3620d9832a66240cf6
Odin <116444054+odinlearner@users.noreply.github.com>
2022-10-28 19:04:58 +0200

    Solve 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = [...string];
+    stringArray.reverse()
+    return stringArray.join("")
 };
 
 // Do not edit below this line
