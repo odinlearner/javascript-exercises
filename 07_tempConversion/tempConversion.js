ce46afd279be28f13bd1675719392f598ac688dd
Odin <116444054+odinlearner@users.noreply.github.com>
2022-10-28 19:31:27 +0200

    Solve 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f_temp) {
+  const c_temp = (f_temp - 32) * (5/9)
+  return Math.round(c_temp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(c_temp) {
+  const f_temp = (c_temp * (9/5)) + 32;
+  return Math.round(f_temp * 10) / 10;
 };
 
 // Do not edit below this line
