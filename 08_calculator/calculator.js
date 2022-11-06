f0022b714466ef81fc0fe853ed872baa12dea0e3
Odin <116444054+odinlearner@users.noreply.github.com>
2022-11-06 10:15:03 +0100

    Solve 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((p,c) => p+c, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((p,c) => p*c, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let res = 1;
+  for(let i = n; i>0; i--) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
