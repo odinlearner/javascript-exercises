3225b18fc845582ddbf1ca72199dccebbc3f7f43
Odin <116444054+odinlearner@users.noreply.github.com>
2022-11-06 10:27:06 +0100

    Solve 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    const sBase = s.toLowerCase().replace(/[^a-z]+/g, "");
+    for (let i=0; i < sBase.length; i++) {
+        if (sBase[i] !== sBase[sBase.length-(i+1)]) return false;
+    }
+    return true
 };
 
 // Do not edit below this line
