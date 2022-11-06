e82d1cfab89e0c8ef3ad0cdee2c635f001dc6de9
Odin <116444054+odinlearner@users.noreply.github.com>
2022-11-06 11:03:53 +0100

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    const yr = new Date().getFullYear();
+    let oldestAge = -1;
+    return people.reduce((prev, cur) => {
+        const age = (cur.yearOfDeath || yr) - cur.yearOfBirth;
+        if (age > oldestAge) {
+            oldestAge = age;
+            return cur;
+        }
+        return prev;
+    },null)
 };
 
 // Do not edit below this line
