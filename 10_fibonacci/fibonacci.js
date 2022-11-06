af0cc663c98fd74327cd4bac8dced7c1f77d824b
Odin <116444054+odinlearner@users.noreply.github.com>
2022-11-06 10:36:03 +0100

    solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let n = Number.parseInt(input);
+    if (n === undefined || n < 0) return "OOPS";
 
+    const aFib = [1,1];
+    for (let i=2; i<n; i++) {
+        aFib.push(aFib[i-2]+aFib[i-1]);
+    }
+    return aFib[n-1];
 };
 
 // Do not edit below this line
